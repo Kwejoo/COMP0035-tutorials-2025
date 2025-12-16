ae5952045c7623ba1cfbf8f037fd4ccae51f8a64
Kwadwo Bonsu <zceckbo@ucl.ac.uk>
2025-12-16 21:32:04 +0000

    Fix typos in type annotations explanation and improve formatting in ParalympicEvent class

--- src/activities/starter/starter_class.py
+++ src/activities/starter/starter_class.py
@@ -12,7 +12,7 @@ class ParalympicEvent:
          register_athlete() Adds an athlete to the list of athletes
 
      """
-
+    
     def __init__(self, name, sport, classification):
         self.name = name
         self.sport = sport
@@ -31,3 +31,34 @@ class ParalympicEvent:
             athlete_name: A string representing the name of the athlete
         """
         self.athletes.append(athlete_name)
+
+
+sprint = ParalympicEvent("100m Sprint", "Athletics", 1)
+
+longJump = ParalympicEvent("Long Jump", "Athletics", 2)
+
+swimming = ParalympicEvent("50m Freestyle", "Swimming", 3)
+
+waterPolo = ParalympicEvent("Water Polo", "Team Sport", 4)
+
+football = ParalympicEvent("5-a-side Football", "Team Sport", 5)
+
+sprint.register_athlete("Athlete A")
+sprint.register_athlete("Athlete B")
+
+
+class Athlete:
+    def __init__(self, name, team, disability):
+        self.name = name
+        self.team = team
+        self.disability = disability
+    
+    def introduce(self):
+        print(
+            f"My name is {self.name}, I represent {self.team}, and I have a "
+            f"{self.disability} disability."
+        )
+
+athlete1 = Athlete("Athlete A", "Country X", "visual impairment")
+
+athlete1.introduce()
